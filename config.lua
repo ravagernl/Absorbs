9b20df502c43dcdb6b1cb24d49b3a94f86bb8160
Ruben Vincenten <rubenvincenten@gmail.com>
2011-04-29 15:27:03 +0200

    Forgot to add config file.

---
+++ config.lua
@@ -0,0 +1,14 @@
+local config = {
+	height = 24,
+	width = 214,
+	spacing = 4,
+	growup = false,
+	font = {
+		path = [[Fonts\ARIALN.TTF]], 
+		size = 12, 
+		style = "OUTLINE"
+	}
+}
+
+local name, ns = ...
+ns.config = config
